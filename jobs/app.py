018f925e21bd6d229df7e4f676ed831a4b647f8e
Dal Price <dallprice@gmail.com>
2020-12-14 13:52:49 -0700

    Update app.py

--- jobs/app.py
+++ jobs/app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, g
+from flask import Flask, render_template, g, request
 import sqlite3
+import datetime
 
 PATH = "db/jobs.sqlite"
 
@@ -48,4 +49,13 @@ def employer(employer_id):
 
 @app.route('/employer/<employer_id>/review')
 def review(employer_id):
-    return render_template('review.html', employer_id=employer_id)
+    if request.method == 'POST':
+        review = request.form['review']
+        rating = request.form['rating']
+        title = request.form['title']
+        status = request.form['status']
+
+        date = datetime.datetime.now().strftime("%m/%d/%Y")
+        execute_sql('INSERT INTO review (review, rating, title, date, status, employer_id) VALUES (?, ?, ?, ?, ?, ?)', (review, rating, title, date, status, employer_id), commit=True)
+
+        return redirect(url_for('employer', employer_id=employer_id))
